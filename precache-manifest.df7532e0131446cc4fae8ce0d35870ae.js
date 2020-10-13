60f0e8c3b587dda4a980141871ba55d2f3225d0b
jannis.grebien <jannisgrebien@web.de>
2020-10-13 17:41:20 +0200

    Updates

---
+++ precache-manifest.df7532e0131446cc4fae8ce0d35870ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24c2b49ed273de3b76ad719b58ad13b",
+    "url": "/smartbrainApp/index.html"
+  },
+  {
+    "revision": "e37d075008d42bcfe203",
+    "url": "/smartbrainApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "184e1f3cfedeb5178794",
+    "url": "/smartbrainApp/static/css/main.94f51f55.chunk.css"
+  },
+  {
+    "revision": "e37d075008d42bcfe203",
+    "url": "/smartbrainApp/static/js/2.d2a95f6b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrainApp/static/js/2.d2a95f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184e1f3cfedeb5178794",
+    "url": "/smartbrainApp/static/js/main.9131b1ed.chunk.js"
+  },
+  {
+    "revision": "c0eda353d0c8f2cc28eb",
+    "url": "/smartbrainApp/static/js/runtime-main.1411d924.js"
+  }
+]);
