63366db86c68edb55f22400be95b5e54ee28731d
jannis.grebien <jannisgrebien@web.de>
2020-10-14 16:12:41 +0200

    add clarifai api and first img functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkFrom';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+
+
+const app = new Clarifai.App({
+  apiKey: '051797b0a31544958281a11d5f6b5c3b'
+ });
+
 
 
 const particlesOptions = {
@@ -20,19 +29,52 @@ const particlesOptions = {
 }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' 
-        params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imgUrl: ''
+    }
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imgUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+      function(response) {
+        console.log(response);
+      },
+      function (err) {
+        //there was an error
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' 
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imgUrl}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
