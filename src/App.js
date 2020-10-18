94edc68e53c4baad079a3c6fd817ec8762559675
jannis.grebien <jannisgrebien@web.de>
2020-10-18 19:06:48 +0200

    changed api endpoints

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imgUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://quiet-bayou-32484.herokuapp.com/imageurl',{
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -100,7 +100,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('http://localhost:3000/image',{
+          fetch('https://quiet-bayou-32484.herokuapp.com/image',{
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -26,7 +26,7 @@ class SignIn extends React.Component {
 
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://quiet-bayou-32484.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
