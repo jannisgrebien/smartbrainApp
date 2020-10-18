6d46081b078014ab1b51acc52fd786fa92217b63
jannis.grebien <jannisgrebien@web.de>
2020-10-18 16:54:47 +0200

    move api functionality to backend

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkFrom';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
 
 
 
-const app = new Clarifai.App({
-  apiKey: '051797b0a31544958281a11d5f6b5c3b'
- });
+
 
 
 
@@ -93,10 +90,14 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imgUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+    fetch('http://localhost:3000/imageurl',{
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if(response) {
           fetch('http://localhost:3000/image',{
